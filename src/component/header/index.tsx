d710fbd7d3011368a177a9326e0cfaa321ce7969
ngao02 <hoangngocnguyen02@gmail.com>
2023-09-16 12:33:19 +0700

    feat: logo

--- src/component/header/index.tsx
+++ src/component/header/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.scss';
-import { Logo } from '../../asset/icon/logo';
+import { Logo } from '../../asset/icon/Logo';
 
 export const Header = () => {
     return (
