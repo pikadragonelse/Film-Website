7dd8baa7dd77182b63fc7dbd542fb415149be654
blognn_287 <83860336+pikadragonelse@users.noreply.github.com>
2023-09-18 11:56:22 +0700

    fix: fix size component

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { App } from './app';
+import { App } from './App';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement,
--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -2,5 +2,9 @@ import { FilmItem } from '../../component/film-item';
 import './index.scss';
 
 export const Home = () => {
-    return <div className="wrapper-home"></div>;
+    return (
+        <div className="wrapper-home">
+            <FilmItem name="" category="" yearOfManufacture={0} poster="" />
+        </div>
+    );
 };
