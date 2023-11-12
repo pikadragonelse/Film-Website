d89077d225df1898e59570a3aa3418dbbdeee3e1
blognn_287 <83860336+pikadragonelse@users.noreply.github.com>
2023-11-12 10:35:30 +0700

    feat: user page, update router version, fix navigation logic

---
+++ src/asset/icon/zalo-pay.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const ZaloPay = () => {
+    return (
+        <img
+            decoding="async"
+            className="wp-image-53220 size-full entered lazyloaded"
+            src="https://cdn.haitrieu.com/wp-content/uploads/2022/10/Logo-ZaloPay-Square.png"
+            alt="Logo ZaloPay Square"
+            width="1200"
+            height="1200"
+            data-lazy-srcset="https://cdn.haitrieu.com/wp-content/uploads/2022/10/Logo-ZaloPay-Square.png 1200w, https://cdn.haitrieu.com/wp-content/uploads/2022/10/Logo-ZaloPay-Square-300x300.png 300w, https://cdn.haitrieu.com/wp-content/uploads/2022/10/Logo-ZaloPay-Square-1024x1024.png 1024w, https://cdn.haitrieu.com/wp-content/uploads/2022/10/Logo-ZaloPay-Square-150x150.png 150w, https://cdn.haitrieu.com/wp-content/uploads/2022/10/Logo-ZaloPay-Square-768x768.png 768w, https://cdn.haitrieu.com/wp-content/uploads/2022/10/Logo-ZaloPay-Square-350x350.png 350w, https://cdn.haitrieu.com/wp-content/uploads/2022/10/Logo-ZaloPay-Square-635x635.png 635w, https://cdn.haitrieu.com/wp-content/uploads/2022/10/Logo-ZaloPay-Square-100x100.png 100w"
+            data-lazy-sizes="(max-width: 1200px) 100vw, 1200px"
+            data-lazy-src="https://cdn.haitrieu.com/wp-content/uploads/2022/10/Logo-ZaloPay-Square.png"
+            data-ll-status="loaded"
+            sizes="(max-width: 1200px) 100vw, 1200px"
+            srcSet="https://cdn.haitrieu.com/wp-content/uploads/2022/10/Logo-ZaloPay-Square.png 1200w, https://cdn.haitrieu.com/wp-content/uploads/2022/10/Logo-ZaloPay-Square-300x300.png 300w, https://cdn.haitrieu.com/wp-content/uploads/2022/10/Logo-ZaloPay-Square-1024x1024.png 1024w, https://cdn.haitrieu.com/wp-content/uploads/2022/10/Logo-ZaloPay-Square-150x150.png 150w, https://cdn.haitrieu.com/wp-content/uploads/2022/10/Logo-ZaloPay-Square-768x768.png 768w, https://cdn.haitrieu.com/wp-content/uploads/2022/10/Logo-ZaloPay-Square-350x350.png 350w, https://cdn.haitrieu.com/wp-content/uploads/2022/10/Logo-ZaloPay-Square-635x635.png 635w, https://cdn.haitrieu.com/wp-content/uploads/2022/10/Logo-ZaloPay-Square-100x100.png 100w"
+        />
+    );
+};
